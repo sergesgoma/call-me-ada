0a83a028bd712357be4f76a4e74bbd119675973e
Alexandra Goma <gomaserges96@gmail.com>
2022-07-03 19:21:44 +0200

    new update

--- messenger-webhook/controllers/mainController.js
+++ messenger-webhook/controllers/mainController.js
@@ -1,4 +1,5 @@
 require("dotenv").config({ path: "../.env" });
+const request = require("request");
 
 const mainController = {
   homePage: async (req, res) => {
@@ -39,6 +40,14 @@ const mainController = {
         // Get the sender PSID
         let sender_psid = webhook_event.sender.id;
         console.log("Sender PSID: " + sender_psid);
+
+        // Check if the event is a message or postback and
+        // pass the event to the appropriate handler function
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message);
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback);
+        }
       });
 
       // Returns a '200 OK' response to all requests
@@ -46,6 +55,49 @@ const mainController = {
     } else {
       // Returns a '404 Not Found' if event is not from a page subscription
       res.sendStatus(404);
+    }
+
+    // Handles messages events
+    function handleMessage(sender_psid, received_message) {
+      let response;
+
+      // Check if the message contains text
+      if (received_message.text) {
+        // Create the payload for a basic text message
+        response = {
+          text: `You sent the message: "${received_message.text}". Now send me an image!`,
+        };
+      }
+
+      // Sends the response message
+      callSendAPI(sender_psid, response);
+    }
+
+    function callSendAPI(sender_psid, response) {
+      // Construct the message body
+      let request_body = {
+        recipient: {
+          id: sender_psid,
+        },
+        message: response,
+      };
+
+      // Send the HTTP request to the Messenger Platform
+      request(
+        {
+          uri: "https://graph.facebook.com/v14.0/me/messages",
+          qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+          method: "POST",
+          json: request_body,
+        },
+        (err, res, body) => {
+          if (!err) {
+            console.log("message sent!");
+          } else {
+            console.error("Unable to send message:" + err);
+          }
+        }
+      );
     }
   },
 };
