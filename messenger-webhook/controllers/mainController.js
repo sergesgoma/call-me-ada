954f56df2ede47b2bc65032eeb1ee7ae520c43aa
Alexandra Goma <gomaserges96@gmail.com>
2022-07-03 20:50:01 +0200

    update

--- messenger-webhook/controllers/mainController.js
+++ messenger-webhook/controllers/mainController.js
@@ -105,21 +105,7 @@ const mainController = {
               template_type: "generic",
               elements: [
                 {
-                  title: "Is this the right picture?",
-                  subtitle: "Tap a button to answer.",
-                  image_url: attachment_url,
-                  buttons: [
-                    {
-                      type: "postback",
-                      title: "Yes!",
-                      payload: "yes",
-                    },
-                    {
-                      type: "postback",
-                      title: "No!",
-                      payload: "no",
-                    },
-                  ],
+                  title: "Je ne sais pas traiter ce type de demande",
                 },
               ],
             },
@@ -157,22 +143,8 @@ const mainController = {
         }
       );
     }
-    function handlePostback(sender_psid, received_postback) {
-      let response;
-
-      // Get the payload for the postback
-      let payload = received_postback.payload;
-
-      // Set the response based on the postback payload
-      if (payload === "yes") {
-        response = { text: "Thanks!" };
-      } else if (payload === "no") {
-        response = { text: "Oops, try sending another image." };
-      }
-      // Send the message to acknowledge the postback
-      callSendAPI(sender_psid, response);
-    }
   },
+
 };
 
 module.exports = mainController;
