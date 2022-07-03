64b115a5bddf4a9cf9a284cebcc50e9455beec73
Alexandra Goma <gomaserges96@gmail.com>
2022-07-03 18:54:33 +0200

    update

--- messenger-webhook/controllers/mainController.js
+++ messenger-webhook/controllers/mainController.js
@@ -31,12 +31,14 @@ const mainController = {
 
     // Checks this is an event from a page subscription
     if (body.object === "page") {
-      // Iterates over each entry - there may be multiple if batched
       body.entry.forEach(function (entry) {
-        // Gets the message. entry.messaging is an array, but
-        // will only ever contain one message, so we get index 0
+        // Gets the body of the webhook event
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
+
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log("Sender PSID: " + sender_psid);
       });
 
       // Returns a '200 OK' response to all requests
